5c6eb66c931273cda606de1b08d88c2e52a87ee5
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-07-27 22:06:41 -0400

    fix: initial working main.tf, ec2 instance and sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,23 @@ data "aws_subnet" "default" {
     id = var.subnet_id
 }
 
+data "aws_key_pair" "surim_site_key" {
+    key_name = "surim_site_key"
+    include_public_key = true
+}
+
 resource "aws_instance" "surim_site" {
-    ami = "ami-0b0dcb5067f272e55"
+    ami = "ami-0862be96e41dcbf74"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.surim_site_sg.id]
-    subnet_id = data.aws_vpc.default.id
+    subnet_id = data.aws_subnet.default.id
     associate_public_ip_address = true
-
+    key_name = data.aws_key_pair.surim_site_key.key_name
+    root_block_device {
+        volume_size = 8
+        volume_type = "gp2"
+    }
+    
     tags = merge(var.surim_site_tags,
         {
             Name = "${var.surim_site_tags["Application"]}_instance"
@@ -28,9 +38,22 @@ resource "aws_security_group" "surim_site_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    egress {
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+    ingress {
         from_port = 3000
         to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+        from_port = 0
+        to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
