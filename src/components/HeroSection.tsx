38cdc113b2a2b7e36e038e0818ba9b8432947cfc
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-21 18:31:07 -0400

    Added HeroSection component for site

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from 'react'
+import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, my name is{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                'Surim Kim',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-lg sm:text-lg lg:text-xl mb-6">
+            I'm a dolphin kind of guy and I like potatos
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+               Download CV
+              </span> 
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            <Image 
+              src="/images/headshot.jpg"
+              alt="surim"
+              className="rounded-full object-fit-cover" 
+              width={300} 
+              height={300} 
+            />         
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
