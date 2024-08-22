f86ee636b6cb966b4edd55ced15ab94fc3e552e2
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-22 00:07:27 -0400

    add AboutSection and TabButton components

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,91 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>skill</li>
+        <li>skill</li>
+        <li>skill</li>
+        <li>skill</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>Bachelor of Science in Bioinformatics</li>
+        <li>University of Pittsburgh, Pittsburgh</li>
+        <li>2018</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Cert</li>
+        <li>Cert</li>
+        <li>Cert</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: string) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text=base lg:text-lg">Blah blah blah blah blah</p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/TabButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface TabButtonProps {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+}
+const TabButton = ({ active, selectTab, children }: TabButtonProps) => {
+  const buttonClasses = active
+    ? "text-white border-b border-purple-500"
+    : "text-[$ADB7BE]";
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default TabButton;
