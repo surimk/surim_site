f324580308bf8d071b0fc2017d6a3e83361c845b
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-22 13:34:41 -0400

    add Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
       </div>
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
+
+const Footer = () => {
+    return (
+      <footer className="bg-slate-1000 text-center p-4 mt-4">
+        <ul className="flex justify-center mt-2">
+          <li className="mr-4">
+            <a
+              href="https://github.com/surimk"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaGithub size={24} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://linkedin.com/in/surimkim"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaLinkedin size={24} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:surimkim@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaEnvelope size={24} />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    );
+  };
+  
+  export default Footer;
