5aa9d34d5491723ca43ef0cab26616d3a34936a1
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-22 16:33:54 -0400

    update HeroSection with framer motion

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
@@ -20,28 +21,54 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-lg sm:text-lg lg:text-xl mb-6">
-            I'm a dolphin kind of guy and I like potatos
-          </p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.175 }}
+          >
+            <p className="text-[#ADB7BE] text-lg sm:text-lg lg:text-xl mb-6">
+            I'm a Software Engineer based in New York City with a Bioinformatics background.
+            </p>
+          </motion.div> 
           <div>
-            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire Me
-            </button>
-            <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </button>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.25 }}
+              className="inline-block"
+            >
+              <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                Hire Me
+              </button>
+            </motion.div>
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="inline-block"
+            > 
+              <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                  Download CV
+                </span>
+              </button>
+            </motion.div>      
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <Image
-            src="/images/headshot.jpg"
-            alt="surim"
-            className="rounded-full object-fit-cover"
-            width={300}
-            height={300}
-          />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <Image
+              src="/images/headshot.jpg"
+              alt="surim"
+              className="rounded-full object-fit-cover "
+              width={300}
+              height={300}
+            />
+          </motion.div>
         </div>
       </div>
     </section>
