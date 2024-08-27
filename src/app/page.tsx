f420b2da6bfa1255dc1e636f5688ea1b159ac511
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-27 17:23:42 -0400

    update Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
@@ -8,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-12">
+      <div className="container mt-24 mx-auto px-12 py-12 flex-grow">
         <HeroSection />
         <AboutSection />
       </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,111 @@
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+"use client";
+import { FaGithub, FaLinkedin, FaEnvelope, FaAws } from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiTerraform,
+  SiDocker,
+  SiGithubactions,
+} from "react-icons/si";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-1000 text-center p-4 mt-4">
-      <ul className="flex justify-center mt-2">
-        <li className="mr-4">
-          <a
-            href="https://github.com/surimk"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 hover:text-white"
-          >
-            <FaGithub size={24} />
-          </a>
-        </li>
-        <li className="mr-4">
-          <a
-            href="https://linkedin.com/in/surimkim"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 hover:text-white"
-          >
-            <FaLinkedin size={24} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="mailto:surimkim@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 hover:text-white"
-          >
-            <FaEnvelope size={24} />
-          </a>
-        </li>
-      </ul>
-    </footer>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <footer className="bg-black text-center p-2 mt-4 sticky bottom-0 bg-opacity-100">
+        <ul className="flex justify-center mt-2">
+          <li className="mr-4">
+            <a
+              href="https://github.com/surimk/surim_site"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaGithub size={24} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://linkedin.com/in/surimkim"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaLinkedin size={24} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:surimkim@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaEnvelope size={24} />
+            </a>
+          </li>
+        </ul>
+
+        <ul className="flex justify-center mt-2">
+          <p className="text-gray-400 text-xs">
+            This site is powered by &nbsp; &nbsp;
+          </p>
+          <li className="mr-4">
+            <a
+              href="https://aws.amazon.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <FaAws size={18} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://nextjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <SiNextdotjs size={18} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://www.terraform.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <SiTerraform size={18} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://www.docker.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <SiDocker size={18} />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="https://github.com/features/actions"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <SiGithubactions size={18} />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </motion.div>
   );
 };
 
