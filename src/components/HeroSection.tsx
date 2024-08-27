daefdcb302810a438c4bc60dce88e414a3023a63
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-26 21:22:34 -0400

    add CV pdf and download link to HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -48,7 +48,15 @@ const HeroSection = () => {
               transition={{ duration: 1, delay: 0.5 }}
               className="md:inline-block flex"
             >
-              <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <button 
+                className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+                onClick={() => {
+                  const link = document.createElement('a');
+                  link.href = '/static/Surim_Kim_CV.pdf';
+                  link.download = 'Surim_Kim_CV.pdf';
+                  link.click();
+                }}
+              >
                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                   Download CV
                 </span>
