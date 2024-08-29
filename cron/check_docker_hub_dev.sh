05d09668e20a79aebf3eb9a2be43c5816826740b
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-29 19:24:06 -0400

    update check_docker_hub_dev.sh

--- cron/check_docker_hub_dev.sh
+++ cron/check_docker_hub_dev.sh
@@ -12,23 +12,23 @@ TAGS=$(curl -s -H "Accept: application/json" https://hub.docker.com/v2/repositor
 # Filter the tags to get the latest one with "dev" in the end
 LATEST_DEV_TAG=$(echo "$TAGS" | jq -r '.results[] | .name' | grep -E 'dev$' | sort -V | tail -1)
 
-# Get the current tag of the running container
-CURRENT_TAG=$(docker inspect -f '{{ .Config.Image }}' ${CONTAINER_NAME} | cut -d ':' -f 2-)
+# Pull the latest image
+PULL_OUTPUT=$(docker pull ${DOCKER_HUB_USERNAME}/${IMAGE_NAME}:${LATEST_DEV_TAG} 2>&1)
 
-# Check if the latest tag is different from the current tag
-if [ "$LATEST_DEV_TAG" != "$CURRENT_TAG" ]; then
-  # If the tags are different, it means there's a newer version of the image
-  echo "Newer version of the image found, updating..."
-
-  # If the container is running, kill it
-  if docker ps -q -f name=${CONTAINER_NAME} > /dev/null; then
-    # If the container exists, kill it
-    docker kill ${CONTAINER_NAME}
-    docker container rm ${CONTAINER_NAME}
-  fi
-
-  # Run the newly pulled image
-  docker run -d -e NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY=$WEB3FORMS_ACCESS_KEY -p 4000:4000 --restart=always --name ${CONTAINER_NAME} ${DOCKER_HUB_USERNAME}/${IMAGE_NAME}:${LATEST_DEV_TAG} npm run start_dev
-else
-  echo "No new version of the image found, skipping update."
+if echo "$PULL_OUTPUT" | grep -q "Image is up to date"; then
+  echo "Image is already up to date, ignoring update..."
+  exit 0
 fi
+
+# If the pull is successful, it means there's a newer version of the image
+echo "Newer version of the image found, updating..."
+
+# If the container is running, kill it
+if docker ps -q -f name=${CONTAINER_NAME} > /dev/null; then
+  # If the container exists, kill it
+  docker kill ${CONTAINER_NAME}
+  docker container rm ${CONTAINER_NAME}
+fi
+
+# Run the newly pulled image
+docker run -d -e NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY=$WEB3FORMS_ACCESS_KEY -p 4000:4000 --restart=always --name ${CONTAINER_NAME} ${DOCKER_HUB_USERNAME}/${IMAGE_NAME}:${LATEST_DEV_TAG} npm run start_dev
