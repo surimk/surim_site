c4f898860abc16e1684de4762d84360c4f06d86b
Surim Kim <surimkim@surims-mbp-2.mynetworksettings.com>
2024-08-29 21:57:31 -0400

    add animations and color to ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,6 +5,7 @@ import {
   AiOutlineCheckCircle,
   AiOutlineCloseCircle,
 } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -68,95 +69,122 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-6 bg-gray-800 rounded-md shadow-md">
-      <h2 className="text-2xl font-semibold text-gray-100 mb-6">
-        Get in Touch
-      </h2>
-      <form onSubmit={handleSubmit} className="space-y-5">
-        <div>
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-300 mb-1"
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="max-w-md mx-auto p-6 bg-black rounded-md shadow-md">
+        <h2 className="text-2xl font-semibold text-gray-100 mb-6">
+          Get in Touch
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-5">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.75 }}
           >
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="w-full px-4 py-2 bg-gray-700 text-gray-100 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Your Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-300 mb-1"
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-300 mb-1"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full px-4 py-2 bg-[#393939] text-gray-100 border border-[#393939] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Your Name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.0 }}
           >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full px-4 py-2 bg-gray-700 text-gray-100 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="you@example.com"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-300 mb-1"
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-300 mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-4 py-2 bg-[#393939] text-gray-100 border border-[#393939] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="you@example.com"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.25 }}
           >
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            rows={5}
-            className="w-full px-4 py-2 bg-gray-700 text-gray-100 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Your Message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-300 mb-1"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={5}
+                className="w-full px-4 py-2 bg-[#393939] text-gray-100 border border-[#393939] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Your Message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.25 }}
+          >
+            <button
+              type="submit"
+              className="w-full px-4 py-2 bg-gradient-to-r from-purple-400 to-pink-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center"
+              disabled={status === "loading"}
+            >
+              {status === "loading" ? (
+                <AiOutlineLoading3Quarters className="animate-spin h-5 w-5 mr-2" />
+              ) : (
+                "Send Message"
+              )}
+            </button>
+          </motion.div>
+        </form>
 
-        <button
-          type="submit"
-          className="w-full px-4 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center"
-          disabled={status === "loading"}
-        >
-          {status === "loading" ? (
-            <AiOutlineLoading3Quarters className="animate-spin h-5 w-5 mr-2" />
-          ) : (
-            "Send Message"
-          )}
-        </button>
-      </form>
+        {status === "success" && (
+          <div className="mt-5 flex items-center text-green-400">
+            <AiOutlineCheckCircle className="h-6 w-6 mr-2" />
+            <span>Your message has been sent successfully!</span>
+          </div>
+        )}
 
-      {status === "success" && (
-        <div className="mt-5 flex items-center text-green-400">
-          <AiOutlineCheckCircle className="h-6 w-6 mr-2" />
-          <span>Your message has been sent successfully!</span>
-        </div>
-      )}
-
-      {status === "error" && (
-        <div className="mt-5 flex items-center text-red-400">
-          <AiOutlineCloseCircle className="h-6 w-6 mr-2" />
-          <span>{errorMessage}</span>
-        </div>
-      )}
-    </div>
+        {status === "error" && (
+          <div className="mt-5 flex items-center text-red-400">
+            <AiOutlineCloseCircle className="h-6 w-6 mr-2" />
+            <span>{errorMessage}</span>
+          </div>
+        )}
+      </div>
+    </motion.div>
   );
 };
 
